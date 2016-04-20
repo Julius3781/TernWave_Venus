6b1c1e62bb1289b6771b680010d15af0a7ccda60
gsary <gmooradi@comcast.net>
2016-04-20 08:54:24 -0400

    My first commit - Modified testNG xml file

---
+++ TernWave/src/twtest/Constants.java
@@ -0,0 +1,25 @@
+package twtest;
+
+public class Constants {
+	
+	// Constants pertaining to Sheet Names and Column names in excel file
+	
+	//Suite
+	public static String TEST_SUITE_SHEET= "Test Suite";
+	public static String Suite_ID="TSID";
+	public static String RUNMODE_YES="Y";
+	public static String RUNMODE="Runmode";
+	
+	//Test Cases
+	public static String TEST_CASES_SHEET="Test Cases";
+	public static String TCID="TCID";
+	public static String TEST_STEPS_SHEET="Test Steps";
+	public static String KEYWORD="Keyword";
+	
+	
+	
+	
+	
+	
+
+}
